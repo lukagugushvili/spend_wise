386d2f41575f4bc15272528c465f34268d5d4025
lukagugushvili <gugushvili58@gmail.com>
2024-10-02 02:41:33 -0700

    Implement AuthGuard in ExpenseController to protect POST route with JWT authentication

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { CreateExpenseDto } from './dto/create-expense.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('expenses')
 export class ExpensesController {
   constructor(private readonly expensesService: ExpensesService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createExpenseDto: CreateExpenseDto) {
     return this.expensesService.create(createExpenseDto);
